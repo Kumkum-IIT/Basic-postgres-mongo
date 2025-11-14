035953b7f2887eb21638fba7391fbe568844e4fa
Kumkum Kwatra <96066188+Kumkum-IIT@users.noreply.github.com>
2025-11-14 14:59:35 +0530

    Change password to empty and add comment
    Updated the password field to be empty and added a comment.

--- postgres-main.py
+++ postgres-main.py
@@ -3,7 +3,7 @@ import psycopg2
 conn = psycopg2.connect(
     dbname='postgres',
     user='postgres',
-    password='kumkumsnakescript',
+    password='', #enter password
     host='localhost',
     port='5432'
 )
